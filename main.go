3813a258b7b95fccec924d203719f90c804ffdfe
Tony Mugendi <tony.mugendi@twigafoods.com>
2024-11-11 23:10:04 +0300

    Data types

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	const remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets now!")
+
+	var userName string
+	var userTickets int
+
+	userName = "John Doe"
+	userTickets = 2
+
+	fmt.Printf("User %v has booked %v tickets\n", userName, userTickets)
+
 }
