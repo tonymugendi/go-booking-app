93dca827739641b497bde3669918bf773f9d8295
Tony Mugendi <tony.mugendi@twigafoods.com>
2024-11-13 22:19:12 +0300

    Add pointers

--- main.go
+++ main.go
@@ -11,12 +11,22 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets now!")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	userName = "John Doe"
-	userTickets = 2
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("User %v has booked %v tickets\n", userName, userTickets)
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
 
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v", firstName, lastName, userTickets, email)
 }
